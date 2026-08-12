34dc1da46c2409854187504389f286f2aa4f57ae
Oleksandra Gurkalo <oleksandra.gurkalo@gmail.com>
2026-08-12 16:34:23 -0400

    feat(Home): Add thumbnail support for highlights and integrate Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout.jsx';
 import Home from './pages/Home/Home.jsx';
+import { Analytics } from "@vercel/analytics/react";
 
 const Portfolio = lazy(() => import('./pages/Portfolio/Portfolio.jsx'));
 const About = lazy(() => import('./pages/About/About.jsx'));
@@ -10,42 +11,45 @@ const Contact = lazy(() => import('./pages/Contact/Contact.jsx'));
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/portfolio"
-          element={
-            <Suspense fallback={null}>
-              <Portfolio />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <Suspense fallback={null}>
-              <About />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/packages"
-          element={
-            <Suspense fallback={null}>
-              <Packages />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/contact"
-          element={
-            <Suspense fallback={null}>
-              <Contact />
-            </Suspense>
-          }
-        />
-      </Route>
-    </Routes>
+    <>
+      <Analytics />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/portfolio"
+            element={
+              <Suspense fallback={null}>
+                <Portfolio />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <Suspense fallback={null}>
+                <About />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/packages"
+            element={
+              <Suspense fallback={null}>
+                <Packages />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <Suspense fallback={null}>
+                <Contact />
+              </Suspense>
+            }
+          />
+        </Route>
+      </Routes>
+    </>
   );
 }
